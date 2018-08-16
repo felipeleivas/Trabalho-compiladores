1a238a5bd9e5c8a356c6c7a60e5ea3565d6b6306
Felipe Leivas <leivascmpa@hotmail.com>
2018-08-16 23:44:35 +0000

    First Commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "lex.yy.h"
+// #include "tokens.h"
+
+int main( int argc, char **argv)
+    {
+    ++argv, --argc;  /* skip over program name */
+    if ( argc > 0 )
+            yyin = fopen( argv[0], "r" );
+    else
+            yyin = stdin;
+
+    yylex();
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+make
+./etapa1 text.file
